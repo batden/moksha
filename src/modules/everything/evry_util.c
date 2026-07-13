b11ceca00c556a9b6b5f1211cd0f5165d3a3b4b8
thewaiter <s.uram@centrum.sk>
2026-07-13 09:00:14 +0200

    Evry fix revert (it is more complex)

--- src/modules/everything/evry_util.c
+++ src/modules/everything/evry_util.c
@@ -1034,7 +1034,6 @@ evry_fuzzy_match(const char *str, const char *match)
                        cnt++;
                        next = p;
                        m_cnt = 0;
-                       pm = NULL;
                        break;
                     }
                }
@@ -1115,7 +1114,6 @@ evry_fuzzy_match(const char *str, const char *match)
                   cnt++;
                   next = p;
                   m_cnt = 0;
-                  pm = NULL;
                   break;
                }
              else
